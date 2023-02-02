03c815b18a7ce7ba518cc3cda6020968b94a5f43
Riccardo-Riva <riccardo.riva1998@gmail.com>
2023-02-02 12:41:17 +0100

    Real Ward Identity

---
+++ TestABISS/ExampleProcess/kira_input/Contribution_4_1.m
@@ -0,0 +1,61 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+((I/16)*(-56 + 46*d - 12*d^2 + d^3)*EL^6*gAl*gAu*gWdu^2*gWNl^2*s12*CKM[1, 1]*
+   Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, -1, 1, 1, 1])/
+  (Pi^4*(MW^2 - s12)) - ((I/32)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   (2*(14 - 8*d + d^2)*s12 + (-4 - 2*d + d^2)*s13 - (32 - 14*d + d^2)*s23)*
+   CKM[1, 1]*Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 0, 0, 1, 1])/
+  (Pi^4*(MW^2 - s12)) - ((I/32)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   (2*(14 - 8*d + d^2)*s12 + (-4 - 2*d + d^2)*s13 - (32 - 14*d + d^2)*s23)*
+   CKM[1, 1]*Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 0, 1, 0, 1])/
+  (Pi^4*(MW^2 - s12)) + ((I/16)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   ((-4 - 2*d + d^2)*s13 - (32 - 14*d + d^2)*s23)*CKM[1, 1]*
+   Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 0, 1, 1, 0])/
+  (Pi^4*(MW^2 - s12)) - ((I/32)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   (-2*(-72 + 54*d - 13*d^2 + d^3)*s12^2 + 8*d*s13^2 - 6*d^2*s13^2 + 
+    d^3*s13^2 + 2*(8 - 6*d + d^2)*s12*(s13 - s23) + 112*s13*s23 - 
+    60*d*s13*s23 + 8*d^2*s13*s23 + 96*s23^2 - 68*d*s23^2 + 14*d^2*s23^2 - 
+    d^3*s23^2 + (-4 + d)*MW^2*(2*(14 - 8*d + d^2)*s12 + 
+      (-4 - 2*d + d^2)*s13 - (32 - 14*d + d^2)*s23))*CKM[1, 1]*
+   Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 0, 1, 1, 1])/
+  (Pi^4*(MW^2 - s12)) - ((I/32)*(-128 + 88*d - 18*d^2 + d^3)*EL^6*gAl*gAu*
+   gWdu^2*gWNl^2*s23*CKM[1, 1]*Conjugate[CKM[1, 1]]*
+   userIntegral[TopoCMp, {MW}, 1, -1, 1, 1])/(Pi^4*(MW^2 - s12)) + 
+ ((I/16)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*(2*(14 - 8*d + d^2)*s12 + 
+    (-4 - 2*d + d^2)*s13)*CKM[1, 1]*Conjugate[CKM[1, 1]]*
+   userIntegral[TopoCMp, {MW}, 1, 0, 0, 1])/(Pi^4*(MW^2 - s12)) - 
+ ((I/32)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*(2*(14 - 8*d + d^2)*s12 + 
+    (-4 - 2*d + d^2)*s13 - (32 - 14*d + d^2)*s23)*CKM[1, 1]*
+   Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 1, 0, 1, 0])/
+  (Pi^4*(MW^2 - s12)) + ((I/32)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   (2*MW^2*(2*(14 - 8*d + d^2)*s12 + (-4 - 2*d + d^2)*s13) - 
+    s23*(-((40 - 18*d + d^2)*s12) + (16 - 6*d + d^2)*s13 + 
+      (32 - 14*d + d^2)*s23))*CKM[1, 1]*Conjugate[CKM[1, 1]]*
+   userIntegral[TopoCMp, {MW}, 1, 0, 1, 1])/(Pi^4*(MW^2 - s12)) - 
+ ((I/32)*(-128 + 88*d - 18*d^2 + d^3)*EL^6*gAl*gAu*gWdu^2*gWNl^2*s23*
+   CKM[1, 1]*Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 1, 1, -1, 1])/
+  (Pi^4*(MW^2 - s12)) - ((I/32)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   (2*(14 - 8*d + d^2)*s12 + (-4 - 2*d + d^2)*s13 - (32 - 14*d + d^2)*s23)*
+   CKM[1, 1]*Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 1, 1, 0, 0])/
+  (Pi^4*(MW^2 - s12)) + ((I/32)*(-4 + d)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   ((24 - 10*d + d^2)*s12*s13 + (24 - 10*d + d^2)*s13^2 - 
+    2*(32 - 14*d + d^2)*MW^2*s23 + 2*(14 - 8*d + d^2)*s12*s23 + 
+    (-4 - 2*d + d^2)*s13*s23 + 4*(-11 + 4*d)*s23^2)*CKM[1, 1]*
+   Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 1, 1, 0, 1])/
+  (Pi^4*(MW^2 - s12)) + ((I/16)*(-56 + 46*d - 12*d^2 + d^3)*EL^6*gAl*gAu*
+   gWdu^2*gWNl^2*s12*CKM[1, 1]*Conjugate[CKM[1, 1]]*
+   userIntegral[TopoCMp, {MW}, 1, 1, 1, -1])/(Pi^4*(MW^2 - s12)) - 
+ ((I/32)*EL^6*gAl*gAu*gWdu^2*gWNl^2*(2*(-4 + d)^2*s12^2 + 
+    (-4 + d)*MW^2*(2*(14 - 8*d + d^2)*s12 + (-4 - 2*d + d^2)*s13 - 
+      (32 - 14*d + d^2)*s23) + (-4 + d)*s12*((28 - 12*d + d^2)*s13 + 
+      (64 - 32*d + 3*d^2)*s23) - 4*(-((-4 + d)*s13^2) + 
+      (8 - 6*d + d^2)*s13*s23 + (8 - 5*d + d^2)*s23^2))*CKM[1, 1]*
+   Conjugate[CKM[1, 1]]*userIntegral[TopoCMp, {MW}, 1, 1, 1, 0])/
+  (Pi^4*(MW^2 - s12)) - ((I/32)*EL^6*gAl*gAu*gWdu^2*gWNl^2*
+   ((-128 + 88*d - 18*d^2 + d^3)*MW^4*s23 - (-4 + d)*MW^2*
+     ((24 - 10*d + d^2)*s12*s13 + (24 - 10*d + d^2)*s13^2 + 
+      2*(14 - 8*d + d^2)*s12*s23 + (-4 - 2*d + d^2)*s13*s23 + 
+      4*(-11 + 4*d)*s23^2) + s23*(-2*(-4 + d)^2*s12^2 - 
+      (-4 + d)*s12*((28 - 12*d + d^2)*s13 + (36 - 16*d + d^2)*s23) + 
+      4*(-((-4 + d)*s13^2) + (8 - 6*d + d^2)*s13*s23 + 
+        (8 - 5*d + d^2)*s23^2)))*CKM[1, 1]*Conjugate[CKM[1, 1]]*
+   userIntegral[TopoCMp, {MW}, 1, 1, 1, 1])/(Pi^4*(MW^2 - s12))
